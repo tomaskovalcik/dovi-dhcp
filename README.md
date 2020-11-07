10537f13cca5b70746d1d4c4831a509361cc53d0
Tomas Kovalcik <tom.kovalchik@gmail.com>
2020-11-07 12:41:05 +0100

    ip addresses are now allocated based on SWITCH ID, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # dovi-dhcp
+
+Development setup:
+
+Change to top level directory.
+
+**run ryu application**: 
+```console
+$ python3 main.py
+```
+**start mininet topology**: 
+```console
+$ sudo mn --topo tree,2 --mac --switch ovsk,protocols=OpenFlow13 --controller=remote,port=3939
+```
+
+
+**How to run dhclient**:
+```console
+mininet> h1 dhclient h1-eth0 -v
+```
+
+**How to release an IP address**: 
+```console
+mininet> h1 dhclient h1-eth0 -r
+```
--- dhcp_server.py
+++ dhcp_server.py
@@ -8,125 +8,172 @@ from controller import Controller
 from ryu.ofproto import ofproto_v1_3, ether
 from ryu.lib.packet import packet, ethernet, dhcp, ipv4, udp
 from ryu.lib import addrconv
+import ipaddress
+
+
+# TODO: databaza pre alokovane IP
+# TODO: pridat pravidla pre switche - aby nechodili duplikaty
+# TODO: ak client uvolni IP adresu, naspat ju pridat do poolu
+# TODO: ak client uz raz dostal IP adresu, tak snazit sa pridat tu istu adresu zase
+# TODO: riadit sa https://tools.ietf.org/html/rfc2131#section-4.1 a minimalne spravit veci co su opisane v sekcii 3.1 a 3.2
+
 
 class DhcpServer(simple_switch_13.SimpleSwitch13):
 
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     _CONTEXTS = {"wsgi": WSGIApplication}
 
-    DISCOVER = 1
-    OFFER = 2
-    REQUEST = 3
-    ACK = 4
+    # TODO: najst lepsi sposob ako toto robit, nepaci sa mi to velmi
     DHCP_SERVER_MAC = "aa:bb:cc:dd:ee:ff"
     DHCP_SERVER_IP = "10.0.0.254"
+
+    s1 = ipaddress.ip_network("192.168.1.0/29")
+    s2 = ipaddress.ip_network("192.168.1.8/29")
+    s3 = ipaddress.ip_network("192.168.1.16/29")
 
     def __init__(self, *args, **kwargs):
         super(DhcpServer, self).__init__(*args, **kwargs)
         wsgi = kwargs["wsgi"]
         wsgi.register(Controller, {Controller.simple_switch_instance_name: self})
 
-        self.messages = {1: "DHCP DISCOVER", 2: "DHCP OFFER", 3: "DHCP REQUEST", 4: "DHCP ACK"}
+        self.messages = {
+            1: "DHCP DISCOVER",
+            2: "DHCP OFFER",
+            3: "DHCP REQUEST",
+            4: "DHCP ACK",
+        }
+
+        # TODO: premysliet databazu, neviem ci jednoduchy dict bude stacit..
+        self.database = {}
+
+        # toto predstavuje temp databazu - teda zatial tam ukladam IP adresy ktore su v procese pridelovania
+        # neviem aky to ma zmysel zatial to necham tak
+        self.temp_offered = {}
+
+        self.s1_pool = [ip for ip in self.s1.hosts()]
+        self.s2_pool = [ip for ip in self.s2.hosts()]
+        self.s3_pool = [ip for ip in self.s3.hosts()]
+
+        self.pools = {1: self.s1_pool, 2: self.s2_pool, 3: self.s3_pool}
+        self.space = {1: self.s1, 2: self.s2, 3: self.s3}
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+        dhcp_packet = pkt.get_protocol(dhcp.dhcp)
 
-        dhcpPacket = None
-        dhcpPacket = pkt.get_protocol(dhcp.dhcp)
+        if dhcp_packet:
+            msg_type = ord(dhcp_packet.options.option_list[0].value)
+            if self.messages.get(msg_type) == "DHCP DISCOVER":
+                self.create_dhcp_offer(dhcp_packet, datapath, in_port)
+            elif self.messages.get(msg_type) == "DHCP REQUEST":
+                self.create_dhcp_ack(dhcp_packet, datapath, in_port)
+            else:
+                raise Exception
 
-        if dhcpPacket:
-            msgType = ord(dhcpPacket.options.option_list[0].value)
-            print(str(msgType)+" Dosiel")
-            if self.messages[msgType] == "DHCP DISCOVER":
-                print("sme tutuuuuuuuu")
-                self.create_dhcp_packet(eth.src, dhcpPacket, datapath, in_port)
-            elif self.messages[msgType] == "DHCP REQUEST":
-                self.recieved_request(eth.src, dhcpPacket, datapath, in_port)
+    def create_dhcp_ack(self, dhcp_packet, dp, port, dst_ip="255.255.255.255"):
 
-    def recieved_request(self, dst_mac,dhcp_lst, dp, port, dst_ip='255.255.255.255'):
+        if self.temp_offered.get(dhcp_packet.xid) is None:
+            return
 
-        #############################
+        subnet_mask = self.space[dp.id].netmask
+        yiaddr = self.temp_offered[dhcp_packet.xid]["yiaddr"]
+        chaddr = self.temp_offered[dhcp_packet.xid]["chaddr"]
+
         pkt = packet.Packet()
-        dhcp_ack_msg_type = b'\x05'
-        subnet_option = dhcp.option(tag=dhcp.DHCP_SUBNET_MASK_OPT,
-                                   value=addrconv.ipv4.text_to_bin('255.255.255.0'))
-        gw_option = dhcp.option(tag=dhcp.DHCP_GATEWAY_ADDR_OPT,
-                                value=addrconv.ipv4.text_to_bin('10.0.0.254'))
-        time_option = dhcp.option(tag=dhcp.DHCP_IP_ADDR_LEASE_TIME_OPT,
-                                  value=b'\xFF\xFF\xFF\xFF')
-        msg_option = dhcp.option(tag=dhcp.DHCP_MESSAGE_TYPE_OPT,
-                                 value=dhcp_ack_msg_type)
-        id_option = dhcp.option(tag=dhcp.DHCP_SERVER_IDENTIFIER_OPT,
-                                value=addrconv.ipv4.text_to_bin('10.0.0.98'))
-        hlen = len(addrconv.mac.text_to_bin(dhcp_lst.chaddr))
-        dns_option = dhcp.option(tag=dhcp.DHCP_DNS_SERVER_ADDR_OPT,
-                                 value=addrconv.ipv4.text_to_bin('4.4.4.4'))
-        options = dhcp.options(option_list=[msg_option, id_option,
-                               time_option, subnet_option, dns_option,
-                               gw_option])
+        dhcp_ack_msg_type = b"\x05"
+        subnet_option = dhcp.option(
+            tag=dhcp.DHCP_SUBNET_MASK_OPT,
+            value=addrconv.ipv4.text_to_bin(subnet_mask),
+        )
+        time_option = dhcp.option(
+            tag=dhcp.DHCP_IP_ADDR_LEASE_TIME_OPT, value=b"\xFF\xFF\xFF\xFF"
+        )
+        msg_option = dhcp.option(
+            tag=dhcp.DHCP_MESSAGE_TYPE_OPT, value=dhcp_ack_msg_type
+        )
+        options = dhcp.options(
+            option_list=[
+                msg_option,
+                time_option,
+                subnet_option,
+            ]
+        )
 
-        hlen = len(addrconv.mac.text_to_bin(dhcp_lst.chaddr))
-        dhcp_pkt = dhcp.dhcp(op=dhcp.DHCP_BOOT_REPLY,
-                             hlen=hlen,
-                             chaddr=dhcp_lst.chaddr,
-                             yiaddr='10.0.0.98',
-                             giaddr=dhcp_lst.giaddr,
-                             xid=dhcp_lst.xid,
-                             options=options)
+        hlen = dhcp_packet.hlen
+        dhcp_pkt = dhcp.dhcp(
+            op=dhcp.DHCP_BOOT_REPLY,
+            hlen=hlen,
+            chaddr=dhcp_packet.chaddr,
+            yiaddr=yiaddr,
+            giaddr=dhcp_packet.giaddr,
+            xid=dhcp_packet.xid,
+            options=options,
+        )
 
-        pkt.add_protocol(ethernet.ethernet(ethertype=ether.ETH_TYPE_IP,
-                                           dst=dst_mac, src=self.DHCP_SERVER_MAC))
+        pkt.add_protocol(
+            ethernet.ethernet(
+                ethertype=ether.ETH_TYPE_IP, dst=chaddr, src=self.DHCP_SERVER_MAC
+            )
+        )
         pkt.add_protocol(ipv4.ipv4(dst=dst_ip, src=self.DHCP_SERVER_IP, proto=17))
         pkt.add_protocol(udp.udp(src_port=67, dst_port=68))
         pkt.add_protocol(dhcp_pkt)
         pkt.serialize()
         self.inject_packet(pkt, dp, port)
 
-        ###############
-        #####dorobit databazu pridelinych IP
-        ##############################
+    def create_dhcp_offer(self, dhcp_packet, dp, port, dst_ip="255.255.255.255"):
+        xid = dhcp_packet.xid  # transaction id
+        chaddr = dhcp_packet.chaddr
+        yiaddr = self.pools[dp.id].pop(-1)
+        self.temp_offered[xid] = {"chaddr": chaddr, "yiaddr": yiaddr}
 
-    def create_dhcp_packet(self, dst_mac,dhcp_lst, dp, port, dst_ip='255.255.255.255'):
         pkt = packet.Packet()
-        dhcp_offer_msg_type = b'\x02'
-        hlen = len(addrconv.mac.text_to_bin(dhcp_lst.chaddr))
-        msg_option = dhcp.option(tag=dhcp.DHCP_MESSAGE_TYPE_OPT,
-                                 value=dhcp_offer_msg_type)
+        dhcp_offer_msg_type = b"\x02"
+        hlen = dhcp_packet.hlen
+
+        msg_option = dhcp.option(
+            tag=dhcp.DHCP_MESSAGE_TYPE_OPT, value=dhcp_offer_msg_type
+        )
         options = dhcp.options(option_list=[msg_option])
 
-        pkt.add_protocol(ethernet.ethernet(ethertype=ether.ETH_TYPE_IP,
-                                           dst=dst_mac, src=self.DHCP_SERVER_MAC))
+        pkt.add_protocol(
+            ethernet.ethernet(
+                ethertype=ether.ETH_TYPE_IP, dst=chaddr, src=self.DHCP_SERVER_MAC
+            )
+        )
+
         pkt.add_protocol(ipv4.ipv4(dst=dst_ip, src=self.DHCP_SERVER_IP, proto=17))
         pkt.add_protocol(udp.udp(src_port=67, dst_port=68))
-        pkt.add_protocol(dhcp.dhcp(hlen=hlen,op=dhcp.DHCP_BOOT_REPLY,
-                                   yiaddr="10.0.0.98",xid = dhcp_lst.xid,giaddr=dhcp_lst.giaddr,chaddr=dst_mac,options=options))
-        ###
-        #yiaddr="10.0.0.98" ---IP pre clienta
-        ###
+        pkt.add_protocol(
+            dhcp.dhcp(
+                hlen=hlen,
+                op=dhcp.DHCP_BOOT_REPLY,
+                yiaddr=yiaddr,
+                xid=dhcp_packet.xid,
+                giaddr=dhcp_packet.giaddr,
+                chaddr=chaddr,
+                options=options,
+            )
+        )
         pkt.serialize()
         self.inject_packet(pkt, dp, port)
 
     def inject_packet(self, pkt, dp, port):
-        if dp is None:
-            dp = self.switches[0]
-
         ofproto = dp.ofproto
-        if port is None:
-            port = ofproto.OFPP_FLOOD
-
         parser = dp.ofproto_parser
         data = pkt.data
         action = [parser.OFPActionOutput(port)]
         out = parser.OFPPacketOut(
-            datapath=dp, buffer_id=ofproto.OFP_NO_BUFFER,
+            datapath=dp,
+            buffer_id=ofproto.OFP_NO_BUFFER,
             in_port=ofproto.OFPP_CONTROLLER,
-            actions=action, data=data)
+            actions=action,
+            data=data,
+        )
         dp.send_msg(out)
