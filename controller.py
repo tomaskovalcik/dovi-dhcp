b93c334bd7ae3d35b2620f354161eff488f4e0c2
Tomas Kovalcik <tom.kovalchik@gmail.com>
2020-11-22 17:05:40 +0100

    lease time working

--- controller.py
+++ controller.py
@@ -1,37 +1,53 @@
 from ryu.app.wsgi import ControllerBase, WSGIApplication, route
 from ryu.app.wsgi import Response
 import json
-import gzip,zlib
+import gzip, zlib
 import requests
 import ipaddress
 
+
 class Controller(ControllerBase):
-    simple_switch_instance_name = 'simple_switch_api_app'
+    simple_switch_instance_name = "simple_switch_api_app"
     global scope_net
 
     def __init__(self, req, link, data, **config):
         super(Controller, self).__init__(req, link, data, **config)
         self.simple_switch_app = data[self.simple_switch_instance_name]
 
-    @route('dhcp_server', '/dashboard', methods=['GET'])
+    @route("dhcp_server", "/dashboard", methods=["GET"])
     def evaluate_results(self, req, **kwargs):
         # this is called at the end of the test
 
-        f = open('index.html')
+        f = open("index.html")
         page = f.read()
-        return Response(content_type='text/html', body=page)
+        return Response(content_type="text/html", body=page)
 
-    @route('dhcp_server', '/data', methods=['POST'])
+    @staticmethod
+    def _split(hex_num):
+        n = hex_num[2:]
+        return [n[i : i + 2] for i in range(0, len(n), 2)]
+
+    def _build(self, hex_num):
+        default = ["00", "00", "00", "00"]
+
+        hex_array = self._split(hex_num)
+        hex_array.reverse()
+        i = -1
+        for b in hex_array:
+            default[i] = b
+            i -= 1
+        return [int(x, 16) for x in default]
+
+    @route("dhcp_server", "/data", methods=["POST"])
     def evaluate_dashboard(self, req, **kwargs):
         # this is called at the end of the test
-        msg = 'Done!'
-        scope_netA = req.POST['networkA']
-        scope_netB = req.POST['networkB']
-        scope_netC = req.POST['networkC']
-        gateway = req.POST['gw']
+        msg = "Done!"
+        scope_netA = req.POST["networkA"]
+        scope_netB = req.POST["networkB"]
+        scope_netC = req.POST["networkC"]
+        gateway = req.POST["gw"]
         dhcp_server = self.simple_switch_app
-        dhcp_server.DHCP_SERVER_IP=gateway
-
+        dhcp_server.DHCP_SERVER_IP = gateway
 
         s1 = ipaddress.ip_network(scope_netA)
         s2 = ipaddress.ip_network(scope_netB)
@@ -44,12 +60,14 @@ class Controller(ControllerBase):
         poolstmp = {1: s1_pool, 2: s2_pool, 3: s3_pool}
         dhcp_server.pools = poolstmp
         spacetmp = {1: s1, 2: s2, 3: s3}
-        dhcp_server.space=spacetmp
+        dhcp_server.space = spacetmp
 
-        lease_time = req.POST['time']
+        lease_time = req.POST["time"]
 
-        dhcp_server.LEASE_TIME_ACK=str(hex(int(lease_time)))
+        if int(lease_time) > 4294967295:
+            raise Exception
 
+        hex_num = str(hex(int(lease_time)))
+        dhcp_server.LEASE_TIME_ACK = self._build(hex_num)
 
         return msg
-
